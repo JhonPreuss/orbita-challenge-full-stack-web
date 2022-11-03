c5de45781ea5e1c28923600dc1631f676358fb83
JP <jonathan.preuss@redes.ufsm.br>
2022-11-03 00:31:34 -0300

    Inicio da estruturação do front-end com vuetif Criação das rotas para consulta da API para busca de dados dos alunos

---
+++ education-group/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-app-bar app dark>
+      <div class="d-flex align-center mr-2">
+        bezKoder
+      </div>
+
+      <v-btn to="/students" text>
+        Lista de Alubos
+      </v-btn>
+
+      <v-btn to="/add" text>
+        Cadastrar
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ education-group/src/router.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import Router from "vue-router";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/students/:id",
+      name: "students-details",
+      component: () => import("./components/Students.vue")
+    },
+    {
+      path: "/add",
+      name: "add",
+      component: () => import("./components/Students.vue")
+    }
+  ]
+});
